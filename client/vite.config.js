b2670109e58f39f1a251042b0e933b08a53d0dec
Amrutanshu <amrutanshumishra607@gmail.com>
2025-01-08 22:06:21 +0530

    mc

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // This maps '@' to your 'src' folder
+      '@': path.resolve(__dirname, 'src'), // Maps '@' to your 'src' folder
     },
   },
   server: {
@@ -14,5 +14,16 @@ export default defineConfig({
   },
   build: {
     target: 'esnext', // Ensure compatibility with modern browsers
+    chunkSizeWarningLimit: 1000, // Increase chunk size limit to 1000 KB to avoid warning
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // Split dependencies into separate chunks
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
   },
 });
