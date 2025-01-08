cbc7468de72056174e581cacc7372092ca41c20a
Amrutanshu <amrutanshumishra607@gmail.com>
2025-01-08 21:54:04 +0530

    third commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // This maps '@' to your 'src' folder
+    },
+  },
   server: {
-    port: 3000, // You can change this port as needed
+    port: 3000,
   },
   build: {
     target: 'esnext', // Ensure compatibility with modern browsers
